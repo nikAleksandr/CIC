5ed2c30f87b1b02a4ff17a3fb0e547dd64139375
unknown <nlyell@.naco.org>
2015-03-27 09:54:58 -0400

    misc. fixes and changes

--- js/main.js
+++ js/main.js
@@ -1063,7 +1063,7 @@ CIC = {}; // main namespace containing functions, to avoid global namespace clut
 		// if showing a "county profile" indicator, show a mini help dialog
 		if (indObjects[0].has_profile) {
 			var text = 'Click once on a county to see their county profile.';
-			if (indObjects[0].name === 'MFA Profiles' || indObjects[0].name === 'Transportation Funding Profiles') text = 'Click once on a county to see their state profile.';
+			if (indObjects[0].name === 'MFA Profiles' || indObjects[0].name === 'Transportation Funding Profiles' || indObjects[0].name === 'Statewide Muni Bonds Profiles' || indObjects[0].name === 'MAP-21 Profiles') text = 'Click once on a county to see their state profile.';
 			noty({
 				type: 'alert',
 				text: '<strong>' + text + '</strong></br></br>Please make sure to enable popups.',
